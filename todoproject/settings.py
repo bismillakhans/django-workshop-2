2d4b97e16cf0dbed1d8a4d81abcf6d57f912db00
bismillakhans <bismillakhans@gmail.com>
2025-02-16 11:14:10 +0530

    feat: update allow host

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY',"2X7qVLcigmlg)h?9$CbDP<i9N5650@}Gu7L}}60~4TSF=e")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
